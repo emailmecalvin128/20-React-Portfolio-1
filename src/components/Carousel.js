5bcb68026fc3af05a49f28d39ce8c11e200229be
Calvin Lee <emailmecalvin@gmail.com>
2020-10-26 20:13:02 -0400

    redo

---
+++ src/components/Carousel.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+import Card from '../components/Card';
+
+import devgrub from '../assets/images/P1.png';
+import youtube from '../assets/images/P2.png';
+import evverest from '../assets/images/P3.png';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+class Carousel extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [
+            //     {
+            //         id: 0,
+            //         title: '',
+            //         subTitle: '',
+            //         imgSrc: ,
+            //         link: '',
+            //         selected: false
+            //     },
+            //     {
+            //         id: 1,
+            //         title: '',
+            //         subTitle: '',
+            //         imgSrc: ,
+            //         link: '',
+            //         selected: false
+            //     },
+            //     {
+            //         id: 2,
+            //         title: '',
+            //         subTitle: '',
+            //         imgSrc:,
+            //         link: '',
+            //         selected: false
+            //     },
+            // ]
+        }
+    }
+
+
+    handleCardClick = (id, card) => {
+
+        let items = [...this.state.items];
+
+        items[id].selected = items[id].selected ? false : true;
+
+        items.forEach(item => {
+            if(item.id !== id) {
+                item.selected = false;
+            }
+        });
+
+        this.setState({
+            items
+        });
+    }
+
+
+    makeItems = (items) => {
+        return items.map(item => {
+            return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
+        })
+    }
+
+
+    render() {
+        return(
+            <Container fluid={true}>
+                <Row className="justify-content-around">
+                    {this.makeItems(this.state.items)}
+                </Row>
+            </Container>
+        );
+    }
+
+}
+
+export default Carousel;
